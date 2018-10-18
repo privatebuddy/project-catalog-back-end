9a9e152b0af8ae21151714bbb43b6f7085c0a11a
Sasin Siriskaowkul <privatebuddy@gmail.com>
2018-10-18 13:13:29 +0700

    First Commit

---
+++ Database/User.py
@@ -0,0 +1,9 @@
+from sqlalchemy import *
+from Database import Base
+
+
+class User(Base):
+    __tablename__ = "User"
+    id  = Column(Integer, primary_key=True)
+    username = Column(Text)
+    password = Column(Text)
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from Database import session, User
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    items = session.query(User).all();
+    return items[0].username
+
+
+if __name__ == '__main__':
+    app.run()
